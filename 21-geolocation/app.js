6091373baa56f66c23d1d3727a8d5f23f925868b
Vicente Ortiz <vicente@MacBook-Pro-de-Vicente.local>
2020-01-11 12:34:38 +0100

    day 21

--- 21-geolocation/app.js
+++ 21-geolocation/app.js
@@ -1,9 +1,14 @@
 const arrow = document.querySelector(".arrow");
-const speed = document.querySelector(".speed");
+const latitude = document.querySelector(".latitude");
+const longitude = document.querySelector(".longitude");
+const altitude = document.querySelector(".altitude");
 
 navigator.geolocation.watchPosition(
   data => {
-    speed.textContent = data.coords.speed;
+    console.log(data.coords);
+    latitude.textContent = data.coords.latitude;
+    longitude.textContent = data.coords.longitude;
+    altitude.textContent = data.coords.altitude;
     arrow.style.transform = `transform: rotate(${data.coords.heading})`;
   },
   e => window.alert("Let me use your sensors first")
